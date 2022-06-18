370bd013db1d2f6aca3d134e19c073d57fa15d42
wei <444980834@qq.com>
2022-06-17 21:37:06 -0400

    change

--- loan.py
+++ loan.py
@@ -22,10 +22,11 @@
 
 
 cash = 200000
-r = 0.02375
+r = 0.0475
 R = 1+r/12
 year = 15
-denominator = sum(R**i for i in range(year*12))
+month = year * 12
+denominator = sum(R**i for i in range(month))
 
 print("denominator {0}".format(denominator))
 print("total cash {0}".format(cash*R**(year*12)))
@@ -35,8 +36,20 @@ cash = 200000
 r = 0.03
 R = 1+r/12
 year = 30
-denominator = sum(R**i for i in range(year*12))
+month = year * 12
+denominator = sum(R**i for i in range(month))
 
 print("denominator {0}".format(denominator))
 print("total cash {0}".format(cash*R**(year*12)))
 print(cash*R**(year*12)/denominator)
+
+cash = 200000
+r = 0.0399
+R = 1+r/12
+year = 53/12
+month = int(year * 12)
+denominator = sum(R**i for i in range(month))
+
+print("denominator {0}".format(denominator))
+print("total cash {0}".format(cash*R**(year*12)))
+print(cash*R**(15*12)/denominator)
